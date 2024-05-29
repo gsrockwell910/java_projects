ff370c29fc0cd177a6803c588b333135b5ecc244
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-05-29 10:44:26 -0400

    Refactor code for Cipher1Demo demo class

--- Cipher1Demo.java
+++ Cipher1Demo.java
@@ -11,11 +11,6 @@ public class Cipher1Demo
              decryptedString = "";
              
       int shuffle = 0;
-      
-      final int MAX_SHUFFLE = 130;
-             
-     //ArrayList<Character> oriCharList = new ArrayList<Character>();
-     //ArrayList<Character> newCharList = new ArrayList<Character>();
              
       //prompt for user input
       System.out.print("Please enter a string to be encrypted: ");
@@ -23,12 +18,6 @@ public class Cipher1Demo
       
       System.out.print("Please enter a number: ");
       shuffle = keyboard.nextInt();
-      
-      while(shuffle > MAX_SHUFFLE)
-      {
-         System.out.println("ERROR: Please enter a number between 32 and 127. Try again: ");
-         shuffle = keyboard.nextInt(); 
-      }
       
       Cipher1 cipher = new Cipher1(shuffle);
       encryptedString = cipher.encrypt(userString);
